8e879a22e002584ade12789db8e89fd9d7c26b87
blakbox23 <boudii30@gmail.com>
2022-12-02 17:20:42 +0300

    initial

---
+++ app/javascript/controllers/form_submission_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="form-submission"
+export default class extends Controller {
+  search() {
+    clearTimeout(this.timeout)
+    this.timeout = setTimeout(() => {
+      this.element.requestSubmit()
+    }, 2000)
+    
+  }
+}
+
