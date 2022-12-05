2511f3512c38371ec4ad72760b319ab51007d4de
blakbox23 <boudii30@gmail.com>
2022-12-05 17:23:47 +0300

    Add user wait time in form submission

--- app/javascript/controllers/form_submission_controller.js
+++ app/javascript/controllers/form_submission_controller.js
@@ -6,7 +6,7 @@ export default class extends Controller {
     clearTimeout(this.timeout)
     this.timeout = setTimeout(() => {
       this.element.requestSubmit()
-    }, 1000)
+    }, 1200)
     
   }
 }
